db61475cebef6e2e5375b2c4dcc57d4a5835800e
changxianglin <281532612@qq.com>
2020-01-12 00:24:17 +0800

    cookie limit

--- src/router/user.js
+++ src/router/user.js
@@ -1,6 +1,13 @@
 const { login } = require('../controller/user')
 const {   SuccessModel, ErrorModel } = require('../model/resModel')
 
+const getCookieExpires = () => {
+  const d = new Date()
+  d.setTime(d.getTime() + (24 * 60 * 60 * 1000))
+  console.log('toGMTString() is  ', d.toDateString())
+  return d.toGMTString()
+}
+
 const handleUserRouter = (req, res) => {
   const method = req.method // GET POST
 
@@ -11,7 +18,7 @@ const handleUserRouter = (req, res) => {
       return result.then(data => {
            if(data.username) {
               // 操作 cookie
-              res.setHeader('Set-Cookie', `username=${data.username};path=/`)
+              res.setHeader('Set-Cookie', `username=${data.username}; path=/; httpOnly; expires=${getCookieExpires()}`)
 
               return new SuccessModel()
            }
@@ -22,7 +29,9 @@ const handleUserRouter = (req, res) => {
   // 登录验证的测试
   if(method ==='GET' && req.path === '/api/user/login-test') {
       if(req.cookie.username) {
-          return Promise.resolve(new SuccessModel())
+          return Promise.resolve(new SuccessModel({
+                username: req.cookie.username
+          }))
       }
       return Promise.resolve(new ErrorModel('尚未登录'))
   }
